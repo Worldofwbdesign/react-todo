9020260c8c08c1b00fc8a77459fe31d6fb6c9898
Worldofwbdesign <zavodzamutov@gmail.com>
2017-05-23 21:13:02 +0300

    add AppTodo component

---
+++ app/components/AppTodo.jsx
@@ -0,0 +1,13 @@
+let React = require('react');
+
+let AppTodo = React.createClass({
+  render() {
+    return (
+      <div>
+        AppTodo.jsx
+      </div>
+    )
+  }
+});
+
+module.exports = AppTodo;
